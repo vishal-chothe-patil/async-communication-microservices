96100351184d1dca159733001277e2c4a64983b5
vishal-chothe-patil <vishalchothe134@gmail.com>
2025-07-24 18:10:35 +0530

    feat: annotate EmiPayment model for JPA support

--- emi-payment-service/src/main/java/com/expertise/kafka/emipayment/example/model/EmiPayment.java
+++ emi-payment-service/src/main/java/com/expertise/kafka/emipayment/example/model/EmiPayment.java
@@ -1,5 +1,9 @@
 package com.expertise.kafka.emipayment.example.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -7,17 +11,20 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+@Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class EmiPayment {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long paymentId;
+
     private String customerId;
     private Double amount;
     private String loanAccountNo;
     private LocalDateTime paymentDate;
-    private String status; // SUCCESS / FAILED
-
+    private String status;
 }
